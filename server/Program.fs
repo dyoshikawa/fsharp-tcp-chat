d548c56fa00954457cca1ae1443a00d005ffd697
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-30 18:18:41 +0900

    fix

--- server/Program.fs
+++ server/Program.fs
@@ -10,7 +10,7 @@ let sendBufToClients (clients: BlockingCollection<Socket>) (own: Socket) (buf: b
 
 [<EntryPoint>]
 let main _argv =
-    let endPoint = new IPEndPoint(IPAddress.Any, 8080)
+    let endPoint = IPEndPoint(IPAddress.Any, 8080)
     let socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
     socket.Bind(endPoint)
     socket.Listen(10)
