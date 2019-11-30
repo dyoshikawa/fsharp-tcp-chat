6d5e2968f86ae351f5f50db037d7ab78d58c429a
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-30 19:32:46 +0900

    fix

--- Client/Program.fs
+++ Client/Program.fs
@@ -14,6 +14,7 @@ let main _argv =
         match inputIp with
         | "" -> IPAddress.Loopback
         | _ -> IPAddress.Parse(inputIp)
+
     let endPoint = IPEndPoint(ip, 8080)
 
     printfn "接続開始"
@@ -25,6 +26,7 @@ let main _argv =
         let rec receive(): unit =
             let buf =
                 [| for _ in 1 .. 10 -> byte (0) |]
+
             let len = socket.Receive(buf)
             printf "%s" <| Encoding.Default.GetString(buf)
             receive()
@@ -34,8 +36,10 @@ let main _argv =
 
     let rec sendText(): int =
         let inputText = stdin.ReadLine()
+
         let sendBuf =
             Encoding.Default.GetBytes(sprintf "%s: %s\r\n" (socket.LocalEndPoint.ToString()) inputText)
+
         let sendResult = socket.Send(sendBuf)
         if sendResult = -1 then
             printfn "送信失敗"
